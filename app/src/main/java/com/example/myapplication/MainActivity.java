0ae5c2240b494c4a2d774e088e6c4d82fbf16396
luandeptrainhat <115625569+luandeptrainhat@users.noreply.github.com>
2023-03-28 22:23:47 +0700

    hien ds giay

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,39 +1,66 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
-import com.example.myapplication.adapter.testadapter;
-import com.example.myapplication.dao.TestDAO;
-import com.example.myapplication.database.Dbhelper;
+import com.example.myapplication.adapter.Giayadapter;
+import com.example.myapplication.adapter.LoaiGiayAdapter;
+import com.example.myapplication.dao.GiayDAO;
+import com.example.myapplication.dao.LoaiGiayDAO;
+import com.example.myapplication.model.LoaiGiay;
 import com.example.myapplication.model.Product;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-     RecyclerView recyclerView;
-     testadapter testAdapter;
+     RecyclerView recyclerView,recyclerViewLoai;
+     Giayadapter testAdapter;
+     LoaiGiayAdapter loaiGiayAdapter;
     ArrayList<Product> list;
-     TestDAO dao;
+     GiayDAO dao;
+     LoaiGiayDAO loaiGiayDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView= findViewById(R.id.recycleview);
-        dao = new TestDAO(MainActivity.this);
+        recyclerViewLoai = findViewById(R.id.recycleviewloai);
 
+
+
+        showloaigiay();
+        showgiay();
+
+
+
+
+    }
+
+    private void showgiay() {
+        dao = new GiayDAO(MainActivity.this);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(linearLayoutManager);
         list = dao.getDSPRO();
-        testAdapter = new testadapter(this,list);
+        testAdapter = new Giayadapter(this,list);
         recyclerView.setAdapter(testAdapter);
+    }
 
+    private void showloaigiay() {
+        ArrayList<LoaiGiay> listloai;
+        loaiGiayDAO = new LoaiGiayDAO(MainActivity.this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
 
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerViewLoai.setLayoutManager(linearLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
+        listloai = loaiGiayDAO.getDSLOAI();
+        loaiGiayAdapter = new LoaiGiayAdapter(this, listloai);
+        recyclerViewLoai.setAdapter(loaiGiayAdapter);
     }
 }
