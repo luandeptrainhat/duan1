8987032071d30d3dcc52095c3f90ad9158d79307
luandeptrainhat <115625569+luandeptrainhat@users.noreply.github.com>
2023-03-30 16:29:47 +0700

    luandeptrai
    hien fragment

--- app/src/main/java/com/example/myapplication/activity/MainKhach.java
+++ app/src/main/java/com/example/myapplication/activity/MainKhach.java
@@ -1,16 +1,29 @@
 package com.example.myapplication.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.example.myapplication.R;
+import com.example.myapplication.fragment.fragmentTheThao;
 
 public class MainKhach extends AppCompatActivity {
-
+LinearLayout fragmentchung;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainkhach);
+
+        fragmentchung = findViewById(R.id.fragmentchung);
+
+        Fragment fragment = new fragmentTheThao();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragmentchung, fragment).commit();
+
+
+
     }
 }
