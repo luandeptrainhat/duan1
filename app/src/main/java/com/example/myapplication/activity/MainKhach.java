e318f90d98f4ecac929d03de03a929b699031de9
huydung7a5 <115625611+huydung7a5@users.noreply.github.com>
2023-03-31 18:55:38 +0700

    Revert "Merge remote-tracking branch 'origin/luan' into dung"
    This reverts commit 36327d881da5ab2b083987d7b9f6c708a8a49713, reversing
    changes made to b938fc564f8b8e2b095f8739acb4aae83f6d99a9.

--- app/src/main/java/com/example/myapplication/activity/MainKhach.java
+++ app/src/main/java/com/example/myapplication/activity/MainKhach.java
@@ -1,81 +1,16 @@
 package com.example.myapplication.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
 
 import com.example.myapplication.R;
-import com.example.myapplication.fragment.fragmentDa;
-import com.example.myapplication.fragment.fragmentLeoNui;
-import com.example.myapplication.fragment.fragmentTheThao;
-import com.example.myapplication.fragment.fragmentThoiTrang;
 
 public class MainKhach extends AppCompatActivity {
-    LinearLayout fragmentchung;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainkhach);
-
-        fragmentchung = findViewById(R.id.fragmentchung);
-        Fragment fragment = new fragmentTheThao();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.fragmentchung, fragment).commit();
-
-
-        hienfragment();
-
-
-    }
-
-    private void hienfragment() {
-        LinearLayout lineartheothao = findViewById(R.id.lineartheothao);
-        LinearLayout linearthoitrang = findViewById(R.id.linearthoitrang);
-        LinearLayout linearda = findViewById(R.id.linearda);
-        LinearLayout linearleonui = findViewById(R.id.linearleonui);
-
-
-
-        linearda.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fragmentchung = findViewById(R.id.fragmentchung);
-                Fragment fragment = new fragmentDa();
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.fragmentchung, fragment).commit();
-            }
-        });
-        linearthoitrang.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fragmentchung = findViewById(R.id.fragmentchung);
-                Fragment fragment = new fragmentThoiTrang();
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.fragmentchung, fragment).commit();
-            }
-        });
-        lineartheothao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fragmentchung = findViewById(R.id.fragmentchung);
-                Fragment fragment = new fragmentTheThao();
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.fragmentchung, fragment).commit();
-            }
-        });
-        linearleonui.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fragmentchung = findViewById(R.id.fragmentchung);
-                Fragment fragment = new fragmentLeoNui();
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.fragmentchung, fragment).commit();
-            }
-        });
     }
 }
