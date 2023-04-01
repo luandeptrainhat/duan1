725dbf75db0bb70755096472984a58f7a012c0a6
huydung7a5 <115625611+huydung7a5@users.noreply.github.com>
2023-04-01 16:52:56 +0700

    tt

--- app/src/main/java/com/example/myapplication/activity/MainKhach.java
+++ app/src/main/java/com/example/myapplication/activity/MainKhach.java
@@ -1,16 +1,64 @@
 package com.example.myapplication.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.myapplication.R;
+import com.example.myapplication.fragment.fragmentDa;
+import com.example.myapplication.fragment.fragmentLeoNui;
+import com.example.myapplication.fragment.fragmentTheThao;
+import com.example.myapplication.fragment.fragmentThoiTrang;
 
 public class MainKhach extends AppCompatActivity {
+    LinearLayout fragmentchung;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainkhach);
+
+        fragmentchung = findViewById(R.id.fragmentchung);
+        Fragment fragment = new fragmentTheThao();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragmentchung, fragment).commit();
+
+
+        hienfragment();
+
+
+    }
+
+    private void hienfragment() {
+        LinearLayout lineartheothao = findViewById(R.id.lineartheothao);
+        LinearLayout linearthoitrang = findViewById(R.id.linearthoitrang);
+
+
+
+
+
+        linearthoitrang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentchung = findViewById(R.id.fragmentchung);
+                Fragment fragment = new fragmentThoiTrang();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.fragmentchung, fragment).commit();
+            }
+        });
+        lineartheothao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentchung = findViewById(R.id.fragmentchung);
+                Fragment fragment = new fragmentTheThao();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.fragmentchung, fragment).commit();
+            }
+        });
+
     }
 }
