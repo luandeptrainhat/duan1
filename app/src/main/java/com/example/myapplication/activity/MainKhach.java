22b667b1ef017acafeccaf2245c5195263bcbfe1
huydung7a5 <115625611+huydung7a5@users.noreply.github.com>
2023-04-01 18:35:12 +0700

    vb12

--- app/src/main/java/com/example/myapplication/activity/MainKhach.java
+++ app/src/main/java/com/example/myapplication/activity/MainKhach.java
@@ -36,11 +36,17 @@ public class MainKhach extends AppCompatActivity {
     private void hienfragment() {
         LinearLayout lineartheothao = findViewById(R.id.lineartheothao);
         LinearLayout linearthoitrang = findViewById(R.id.linearthoitrang);
+<<<<<<< Updated upstream
         LinearLayout linearda = findViewById(R.id.linearda);
         LinearLayout linearleonui = findViewById(R.id.linearleonui);
 
 
 
+=======
+
+
+
+>>>>>>> Stashed changes
         linearda.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,6 +56,10 @@ public class MainKhach extends AppCompatActivity {
                 fragmentManager.beginTransaction().replace(R.id.fragmentchung, fragment).commit();
             }
         });
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
         linearthoitrang.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,6 +78,10 @@ public class MainKhach extends AppCompatActivity {
                 fragmentManager.beginTransaction().replace(R.id.fragmentchung, fragment).commit();
             }
         });
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
         linearleonui.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,5 +91,77 @@ public class MainKhach extends AppCompatActivity {
                 fragmentManager.beginTransaction().replace(R.id.fragmentchung, fragment).commit();
             }
         });
+<<<<<<< Updated upstream
+=======
+
+
+    private void SetColer(LinearLayout linearLayout) {
+        linearLayout.setBackground(getDrawable(R.drawable.shapemain2));
+      //  linearLayout.setLayoutParams(new LinearLayout.LayoutParams(130, 65));
+        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        linearLayout.setGravity(CENTER);
+      //  linearLayout.setPadding(5, 5, 5, 5);
+
+        for (int i = 0; i < linearLayout.getChildCount(); i++) {
+            View v = linearLayout.getChildAt(i);
+            if (v instanceof TextView) {
+                ((TextView) v).setTextColor(Color.WHITE);
+                ((TextView) v).setTextSize(10);
+
+            }
+
+        }
+    }
+
+    private void Set() {
+        LinearLayout lineartheothao = findViewById(R.id.lineartheothao);
+        LinearLayout linearthoitrang = findViewById(R.id.linearthoitrang);
+        LinearLayout linearda = findViewById(R.id.linearda);
+        LinearLayout linearleonui = findViewById(R.id.linearleonui);
+
+        lineartheothao.setBackground(getDrawable(R.drawable.shapemain3));
+        linearthoitrang.setBackground(getDrawable(R.drawable.shapemain3));
+        linearda.setBackground(getDrawable(R.drawable.shapemain3));
+        linearleonui.setBackground(getDrawable(R.drawable.shapemain3));
+
+        for (int i = 0; i < lineartheothao.getChildCount(); i++) {
+            View v = lineartheothao.getChildAt(i);
+            if (v instanceof TextView) {
+                ((TextView) v).setTextColor(Color.BLACK);
+                ((TextView) v).setTextSize(7);
+            }
+
+        }
+        for (int i = 0; i < linearthoitrang.getChildCount(); i++) {
+            View v = linearthoitrang.getChildAt(i);
+            if (v instanceof TextView) {
+                ((TextView) v).setTextColor(Color.BLACK);
+                ((TextView) v).setTextSize(7);
+            }
+
+        }
+        for (int i = 0; i < linearda.getChildCount(); i++) {
+            View v = linearda.getChildAt(i);
+            if (v instanceof TextView) {
+                ((TextView) v).setTextColor(Color.BLACK);
+                ((TextView) v).setTextSize(7);
+            }
+
+        }
+        for (int i = 0; i < linearleonui.getChildCount(); i++) {
+            View v = linearleonui.getChildAt(i);
+            if (v instanceof TextView) {
+                ((TextView) v).setTextColor(Color.BLACK);
+                ((TextView) v).setTextSize(7);
+            }
+
+        }
+
+        lineartheothao.setOrientation(LinearLayout.VERTICAL);
+        linearthoitrang.setOrientation(LinearLayout.VERTICAL);
+        linearda.setOrientation(LinearLayout.VERTICAL);
+        linearleonui.setOrientation(LinearLayout.VERTICAL);
+
+>>>>>>> Stashed changes
     }
 }
