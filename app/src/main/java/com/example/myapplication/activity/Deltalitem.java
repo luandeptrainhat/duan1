f60ff0d2370fd3ad528b4dc40eaf769a678a55d4
huydung7a5 <115625611+huydung7a5@users.noreply.github.com>
2023-04-10 20:05:12 +0700

    dungmoine
    123

--- app/src/main/java/com/example/myapplication/activity/Deltalitem.java
+++ app/src/main/java/com/example/myapplication/activity/Deltalitem.java
@@ -1,17 +1,25 @@
 package com.example.myapplication.activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.icu.text.Transliterator;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +30,7 @@ import com.example.myapplication.adapter.GioHangAdapter;
 import com.example.myapplication.dao.GiayDAO;
 import com.example.myapplication.model.ItemGioHang;
 import com.example.myapplication.model.Product;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
 
@@ -30,11 +39,14 @@ public class Deltalitem extends AppCompatActivity {
 
       ArrayList<Product> list;
        Button btndathang;
+       BottomSheetDialog dialog;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motasanpham);
-
+        btndathang = findViewById(R.id.btndathang);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         Intent intent = getIntent();
@@ -46,22 +58,58 @@ public class Deltalitem extends AppCompatActivity {
         TextView txtgia = findViewById(R.id.txtgia);
         txtgia.setText(String.valueOf(product.getGia()));
 
-        dao = new GiayDAO(Deltalitem.this);
-         btndathang = findViewById(R.id.btndathang);
-         btndathang.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 SharedPreferences sharedPreferences = getSharedPreferences("THONGTIN",MODE_PRIVATE);
-                 String tk = sharedPreferences.getString("taikhoan",null);
-                 if (dao.themVaoGH(product.getMagiay(),1,tk)){
-                     Toast.makeText(Deltalitem.this, "thêm vào giỏ hàng thành công", Toast.LENGTH_SHORT).show();
-                 }else {
-                     Toast.makeText(Deltalitem.this, "thất bại", Toast.LENGTH_SHORT).show();
-                 }
-             }
-         });
+         dao = new GiayDAO(Deltalitem.this);
+         dialog = new BottomSheetDialog(this);
+         showdialog();
+
+
+          btndathang.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+         dialog.show();
+         }
+     });
+          dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
 
     }
+ public  void showdialog(){
+View view = getLayoutInflater().inflate(R.layout.layoutmotasanpham, null, false);
+
+     CardView btnthemvaogiohang = view.findViewById(R.id.layaothemvaogiohang);
+     ImageButton imgexit = view.findViewById(R.id.imgexit);
+
+     Intent intent = getIntent();
+     Bundle bundle = intent.getExtras();
+     Product product = (Product) bundle.getSerializable("truyenne");
+     TextView txtmausac = view.findViewById(R.id.txtMausac);
+     txtmausac.setText(product.getMausac());
+     TextView txtsize1 = view.findViewById(R.id.txtsize1);
+     txtsize1.setText(String.valueOf(product.getKichco()));
+     btnthemvaogiohang.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             SharedPreferences sharedPreferences = getSharedPreferences("THONGTIN",MODE_PRIVATE);
+             String tk = sharedPreferences.getString("taikhoan",null);
+             if (dao.themVaoGH(product.getMagiay(),1,tk)){
+                 Toast.makeText(Deltalitem.this, "thêm vào giỏ hàng thành công", Toast.LENGTH_SHORT).show();
+             }else {
+                 Toast.makeText(Deltalitem.this, "thất bại", Toast.LENGTH_SHORT).show();
+             }
+         }
+     });
+     imgexit.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+
+             dialog.cancel();
+
+         }
+     });
+
+
+dialog.setContentView(view);
+
+ }
 
 }
 
