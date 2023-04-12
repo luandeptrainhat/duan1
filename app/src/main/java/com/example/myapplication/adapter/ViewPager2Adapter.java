b27e4a2cda4db00055050409c99819d3b4f0ebb2
Hoangthanh <thanhnho270701@gmail.com>
2023-04-12 22:03:57 +0700

    xong

--- app/src/main/java/com/example/myapplication/adapter/ViewPager2Adapter.java
+++ app/src/main/java/com/example/myapplication/adapter/ViewPager2Adapter.java
@@ -3,10 +3,8 @@ package com.example.myapplication.adapter;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-import com.example.myapplication.fragment.FragmentLichSuDonHangAdmin;
 import com.example.myapplication.fragment.FragmentXuLiDonHang;
 
 public class ViewPager2Adapter extends FragmentStateAdapter {
@@ -20,8 +18,7 @@ public class ViewPager2Adapter extends FragmentStateAdapter {
         switch (position){
             case 0 :
                 return new FragmentXuLiDonHang();
-            case 1:
-                return new FragmentLichSuDonHangAdmin();
+
             default:
                 return new FragmentXuLiDonHang();
         }
